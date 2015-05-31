724372cacf67a68aad8c67ec371be2cbc014697b
Yangxinye Yang <yangyangxinye@gmail.com>
2015-05-31 13:03:24 -0500

    testing

--- main/models.py
+++ main/models.py
@@ -48,13 +48,11 @@ class Story(models.Model):
         poster = Poster(username=originalPoster, points=100)
         poster.save()
         story = Story(type=type, title=title, content=content, location=loc1, originalPoster=poster)
-        tag = Tag(name='food')
-        tag.save()
-        story.tags.add(tag)
-        # for t in tagList:
-        #     tag = Tag(name=t)
-        #     tag.save()
-        #     story.tags.add(tag)
+        story.save()
+        for t in tagList:
+            tag = Tag(name=t)
+            tag.save()
+            story.tags.add(tag)
         story.save()
 
 
